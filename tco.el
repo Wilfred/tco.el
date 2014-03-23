340e41369e3cea4bf39d424822afd05d0cc47250
Wilfred Hughes <me@wilfred.me.uk>
2014-03-23 09:37:27 +0000

    Specify required Emacs version (24, since we require lexical binding).

--- tco.el
+++ tco.el
@@ -4,7 +4,7 @@
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Version: 0.2
-;; Package-Requires: ((dash "1.2.0"))
+;; Package-Requires: ((dash "1.2.0") (emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@
 
 ;; It works by replacing each self-call with a thunk, and wrapping the
 ;; function body in a loop that repeatedly evaluates the thunk.  Roughly
-;; speaking, a function `foo`:
+;; speaking, a function `foo':
 
 ;; \(defun-tco foo (...)
 ;;   (...)
